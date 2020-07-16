c10abb08867f70183903d22372fa921f21df811d
Ankz29 <44980891+Ankz29@users.noreply.github.com>
2020-07-16 10:54:59 +0530

    Adding files for Runtime Polymorphism ex.

---
+++ Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RunTimePolymorphism;
+
+namespace RunTimePolymorphism
+{
+    public class Animal 
+    {
+        public string Types { get; set; }
+        public Animal()
+        {
+            Types = "Domestic Animals";
+        }
+        public virtual void Sound()
+        {
+            Console.WriteLine("Parent Class called. ");
+        }
+    }
+}
---
+++ Horse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTimePolymorphism
+{
+    public class Horse : Animal 
+    {
+        public string Name { get; set; }
+        public Horse()
+        {
+            Name = "White Knight";
+            Console.WriteLine(Name);
+        }
+        
+        
+        public override void Sound()
+        {
+            Console.WriteLine("Child class called. ");
+        }
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            a.Sound();
+            Console.ReadLine();
+        }
+    }
+}
